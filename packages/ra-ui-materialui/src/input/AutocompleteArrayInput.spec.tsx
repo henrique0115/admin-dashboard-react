marmelab/react-admin

Cleanup is automatically called in the new testing library version

packages/ra-ui-materialui/src/input/AutocompleteArrayInput.spec.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {
-    cleanup,
-    fireEvent,
-    render,
-    waitForDomChange,
-} from '@testing-library/react';
+import { fireEvent, render, waitForDomChange } from '@testing-library/react';
 import { Form } from 'react-final-form';
 import expect from 'expect';
 
@@ -12,8 +7,6 @@ import AutocompleteArrayInput from './AutocompleteArrayInput';
 import { TestTranslationProvider } from 'ra-core';
 
 describe('<AutocompleteArrayInput />', () => {
-    afterEach(cleanup);
-
     const defaultProps = {
         source: 'tags',
         resource: 'posts',

henrique0115/admin-dashboard-react

packages/ra-ui-materialui/src/input/AutocompleteArrayInput.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render, wait, cleanup } from '@testing-library/react';
+import { fireEvent, render, wait } from '@testing-library/react';
 import { Form } from 'react-final-form';
 import expect from 'expect';
 
@@ -11,8 +11,6 @@ describe('<AutocompleteArrayInput />', () => {
         source: 'tags',
         resource: 'posts',
     };
-
-    afterEach(cleanup);
 
     it('should extract suggestions from choices', () => {
         const { getByLabelText, getByText, queryAllByRole } = render(