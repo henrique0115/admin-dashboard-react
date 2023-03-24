marmelab/react-admin

Add server side validation support
## Problem

- Server side validation is cumbersome to set up
- When using middlewares and as the SaveButton relied on the SaveContext.saving prop, the SaveButton was only disabled for while the main mutation was loading. However, additional work may still be running.

## Solution

For `pessismistic` mode only, we now transparently support server side validation. When a server validation occurs, the DataProvider should throw a ServerValidationError that contains an `errors` object matching the form shape.

Besides, we now await the mutation in `useCreateController` and `useEditController`. In `pessimistic` mode, that means the `SaveButton` will stay disabled until the mutation is resolved including its middlewares if any.

That does not change anything for the other mutation modes.

packages/ra-core/src/controller/create/useCreateController.ts
@@ -6,7 +6,11 @@ import { Location } from 'history';
 import { UseMutationOptions } from 'react-query';
 
 import { useAuthenticated } from '../../auth';
-import { useCreate, UseCreateMutateParams } from '../../dataProvider';
+import {
+    HttpError,
+    useCreate,
+    UseCreateMutateParams,
+} from '../../dataProvider';
 import { useRedirect, RedirectionSideEffect } from '../../routing';
 import { useNotify } from '../../notification';
 import { SaveContextValue, useMutationMiddlewares } from '../saveContext';
@@ -74,7 +78,7 @@ export const useCreateController = <
     const [create, { isLoading: saving }] = useCreate<
         RecordType,
         MutationOptionsError
-    >(resource, undefined, otherMutationOptions);
+    >(resource, undefined, { ...otherMutationOptions, returnPromise: true });
 
     const save = useCallback(
         (
@@ -91,55 +95,67 @@ export const useCreateController = <
                     : transform
                     ? transform(data)
                     : data
-            ).then((data: Partial<RecordType>) => {
+            ).then(async (data: Partial<RecordType>) => {
                 const mutate = getMutateWithMiddlewares(create);
-                mutate(
-                    resource,
-                    { data, meta },
-                    {
-                        onSuccess: async (data, variables, context) => {
-                            if (onSuccessFromSave) {
-                                return onSuccessFromSave(
-                                    data,
-                                    variables,
-                                    context
-                                );
-                            }
-                            if (onSuccess) {
-                                return onSuccess(data, variables, context);
-                            }
+                try {
+                    await mutate(
+                        resource,
+                        { data, meta },
+                        {
+                            onSuccess: async (data, variables, context) => {
+                                if (onSuccessFromSave) {
+                                    return onSuccessFromSave(
+                                        data,
+                                        variables,
+                                        context
+                                    );
+                                }
+                                if (onSuccess) {
+                                    return onSuccess(data, variables, context);
+                                }
 
-                            notify('ra.notification.created', {
-                                type: 'info',
-                                messageArgs: { smart_count: 1 },
-                            });
-                            redirect(finalRedirectTo, resource, data.id, data);
-                        },
-                        onError: onErrorFromSave
-                            ? onErrorFromSave
-                            : onError
-                            ? onError
-                            : (error: Error) => {
-                                  notify(
-                                      typeof error === 'string'
-                                          ? error
-                                          : error.message ||
-                                                'ra.notification.http_error',
-                                      {
-                                          type: 'warning',
-                                          messageArgs: {
-                                              _:
-                                                  typeof error === 'string'
-                                                      ? error
-                                                      : error && error.message
-                                                      ? error.message
-                                                      : undefined,
-                                          },
-                                      }
-                                  );
-                              },
+                                notify('ra.notification.created', {
+                                    type: 'info',
+                                    messageArgs: { smart_count: 1 },
+                                });
+                                redirect(
+                                    finalRedirectTo,
+                                    resource,
+                                    data.id,
+                                    data
+                                );
+                            },
+                            onError: onErrorFromSave
+                                ? onErrorFromSave
+                                : onError
+                                ? onError
+                                : (error: Error) => {
+                                      notify(
+                                          typeof error === 'string'
+                                              ? error
+                                              : error.message ||
+                                                    'ra.notification.http_error',
+                                          {
+                                              type: 'warning',
+                                              messageArgs: {
+                                                  _:
+                                                      typeof error === 'string'
+                                                          ? error
+                                                          : error &&
+                                                            error.message
+                                                          ? error.message
+                                                          : undefined,
+                                              },
+                                          }
+                                      );
+                                  },
+                        }
+                    );
+                } catch (error) {
+                    if ((error as HttpError).body.errors != null) {
+                        return (error as HttpError).body.errors;
                     }
-                );
+                }
             }),
         [
             create,

packages/ra-core/src/controller/edit/useEditController.ts
@@ -12,6 +12,7 @@ import {
     useRefresh,
     UseGetOneHookValue,
     UseUpdateMutateParams,
+    HttpError,
 } from '../../dataProvider';
 import { useTranslate } from '../../i18n';
 import { useResourceContext, useGetResourceLabel } from '../../core';
@@ -113,7 +114,11 @@ export const useEditController = <
     const [update, { isLoading: saving }] = useUpdate<
         RecordType,
         MutationOptionsError
-    >(resource, recordCached, { ...otherMutationOptions, mutationMode });
+    >(resource, recordCached, {
+        ...otherMutationOptions,
+        mutationMode,
+        returnPromise: mutationMode === 'pessimistic',
+    });
 
     const save = useCallback(
         (
@@ -134,57 +139,65 @@ export const useEditController = <
                           previousData: recordCached.previousData,
                       })
                     : data
-            ).then((data: Partial<RecordType>) => {
+            ).then(async (data: Partial<RecordType>) => {
                 const mutate = getMutateWithMiddlewares(update);
-                return mutate(
-                    resource,
-                    { id, data, meta: mutationMeta },
-                    {
-                        onSuccess: async (data, variables, context) => {
-                            if (onSuccessFromSave) {
-                                return onSuccessFromSave(
-                                    data,
-                                    variables,
-                                    context
-                                );
-                            }
 
-                            if (onSuccess) {
-                                return onSuccess(data, variables, context);
-                            }
+                try {
+                    await mutate(
+                        resource,
+                        { id, data, meta: mutationMeta },
+                        {
+                            onSuccess: async (data, variables, context) => {
+                                if (onSuccessFromSave) {
+                                    return onSuccessFromSave(
+                                        data,
+                                        variables,
+                                        context
+                                    );
+                                }
 
-                            notify('ra.notification.updated', {
-                                type: 'info',
-                                messageArgs: { smart_count: 1 },
-                                undoable: mutationMode === 'undoable',
-                            });
-                            redirect(redirectTo, resource, data.id, data);
-                        },
-                        onError: onErrorFromSave
-                            ? onErrorFromSave
-                            : onError
-                            ? onError
-                            : (error: Error | string) => {
-                                  notify(
-                                      typeof error === 'string'
-                                          ? error
-                                          : error.message ||
-                                                'ra.notification.http_error',
-                                      {
-                                          type: 'warning',
-                                          messageArgs: {
-                                              _:
-                                                  typeof error === 'string'
-                                                      ? error
-                                                      : error && error.message
-                                                      ? error.message
-                                                      : undefined,
-                                          },
-                                      }
-                                  );
-                              },
+                                if (onSuccess) {
+                                    return onSuccess(data, variables, context);
+                                }
+
+                                notify('ra.notification.updated', {
+                                    type: 'info',
+                                    messageArgs: { smart_count: 1 },
+                                    undoable: mutationMode === 'undoable',
+                                });
+                                redirect(redirectTo, resource, data.id, data);
+                            },
+                            onError: onErrorFromSave
+                                ? onErrorFromSave
+                                : onError
+                                ? onError
+                                : (error: Error | string) => {
+                                      notify(
+                                          typeof error === 'string'
+                                              ? error
+                                              : error.message ||
+                                                    'ra.notification.http_error',
+                                          {
+                                              type: 'warning',
+                                              messageArgs: {
+                                                  _:
+                                                      typeof error === 'string'
+                                                          ? error
+                                                          : error &&
+                                                            error.message
+                                                          ? error.message
+                                                          : undefined,
+                                              },
+                                          }
+                                      );
+                                  },
+                        }
+                    );
+                } catch (error) {
+                    if ((error as HttpError).body.errors != null) {
+                        return (error as HttpError).body.errors;
                     }
-                );
+                }
             }),
         [
             id,

henrique0115/admin-dashboard-react

packages/ra-core/src/controller/create/useCreateController.ts
@@ -6,7 +6,11 @@ import { Location } from 'history';
 import { UseMutationOptions } from 'react-query';
 
 import { useAuthenticated } from '../../auth';
-import { useCreate, UseCreateMutateParams } from '../../dataProvider';
+import {
+    HttpError,
+    useCreate,
+    UseCreateMutateParams,
+} from '../../dataProvider';
 import { useRedirect, RedirectionSideEffect } from '../../routing';
 import { useNotify } from '../../notification';
 import { SaveContextValue, useMutationMiddlewares } from '../saveContext';
@@ -74,7 +78,7 @@ export const useCreateController = <
     const [create, { isLoading: saving }] = useCreate<
         RecordType,
         MutationOptionsError
-    >(resource, undefined, otherMutationOptions);
+    >(resource, undefined, { ...otherMutationOptions, returnPromise: true });
 
     const save = useCallback(
         (
@@ -91,55 +95,67 @@ export const useCreateController = <
                     : transform
                     ? transform(data)
                     : data
-            ).then((data: Partial<RecordType>) => {
+            ).then(async (data: Partial<RecordType>) => {
                 const mutate = getMutateWithMiddlewares(create);
-                mutate(
-                    resource,
-                    { data, meta },
-                    {
-                        onSuccess: async (data, variables, context) => {
-                            if (onSuccessFromSave) {
-                                return onSuccessFromSave(
-                                    data,
-                                    variables,
-                                    context
-                                );
-                            }
-                            if (onSuccess) {
-                                return onSuccess(data, variables, context);
-                            }
+                try {
+                    await mutate(
+                        resource,
+                        { data, meta },
+                        {
+                            onSuccess: async (data, variables, context) => {
+                                if (onSuccessFromSave) {
+                                    return onSuccessFromSave(
+                                        data,
+                                        variables,
+                                        context
+                                    );
+                                }
+                                if (onSuccess) {
+                                    return onSuccess(data, variables, context);
+                                }
 
-                            notify('ra.notification.created', {
-                                type: 'info',
-                                messageArgs: { smart_count: 1 },
-                            });
-                            redirect(finalRedirectTo, resource, data.id, data);
-                        },
-                        onError: onErrorFromSave
-                            ? onErrorFromSave
-                            : onError
-                            ? onError
-                            : (error: Error | string) => {
-                                  notify(
-                                      typeof error === 'string'
-                                          ? error
-                                          : error.message ||
-                                                'ra.notification.http_error',
-                                      {
-                                          type: 'error',
-                                          messageArgs: {
-                                              _:
-                                                  typeof error === 'string'
-                                                      ? error
-                                                      : error && error.message
-                                                      ? error.message
-                                                      : undefined,
-                                          },
-                                      }
-                                  );
-                              },
+                                notify('ra.notification.created', {
+                                    type: 'info',
+                                    messageArgs: { smart_count: 1 },
+                                });
+                                redirect(
+                                    finalRedirectTo,
+                                    resource,
+                                    data.id,
+                                    data
+                                );
+                            },
+                            onError: onErrorFromSave
+                                ? onErrorFromSave
+                                : onError
+                                ? onError
+                                : (error: Error) => {
+                                      notify(
+                                          typeof error === 'string'
+                                              ? error
+                                              : error.message ||
+                                                    'ra.notification.http_error',
+                                          {
+                                              type: 'error',
+                                              messageArgs: {
+                                                  _:
+                                                      typeof error === 'string'
+                                                          ? error
+                                                          : error &&
+                                                            error.message
+                                                          ? error.message
+                                                          : undefined,
+                                              },
+                                          }
+                                      );
+                                  },
+                        }
+                    );
+                } catch (error) {
+                    if ((error as HttpError).body.errors != null) {
+                        return (error as HttpError).body.errors;
                     }
-                );
+                }
             }),
         [
             create,

packages/ra-core/src/controller/edit/useEditController.ts
@@ -12,6 +12,7 @@ import {
     useRefresh,
     UseGetOneHookValue,
     UseUpdateMutateParams,
+    HttpError,
 } from '../../dataProvider';
 import { useTranslate } from '../../i18n';
 import {
@@ -123,7 +124,11 @@ export const useEditController = <
     const [update, { isLoading: saving }] = useUpdate<
         RecordType,
         MutationOptionsError
-    >(resource, recordCached, { ...otherMutationOptions, mutationMode });
+    >(resource, recordCached, {
+        ...otherMutationOptions,
+        mutationMode,
+        returnPromise: mutationMode === 'pessimistic',
+    });
 
     const save = useCallback(
         (
@@ -144,57 +149,65 @@ export const useEditController = <
                           previousData: recordCached.previousData,
                       })
                     : data
-            ).then((data: Partial<RecordType>) => {
+            ).then(async (data: Partial<RecordType>) => {
                 const mutate = getMutateWithMiddlewares(update);
-                return mutate(
-                    resource,
-                    { id, data, meta: mutationMeta },
-                    {
-                        onSuccess: async (data, variables, context) => {
-                            if (onSuccessFromSave) {
-                                return onSuccessFromSave(
-                                    data,
-                                    variables,
-                                    context
-                                );
-                            }
 
-                            if (onSuccess) {
-                                return onSuccess(data, variables, context);
-                            }
+                try {
+                    await mutate(
+                        resource,
+                        { id, data, meta: mutationMeta },
+                        {
+                            onSuccess: async (data, variables, context) => {
+                                if (onSuccessFromSave) {
+                                    return onSuccessFromSave(
+                                        data,
+                                        variables,
+                                        context
+                                    );
+                                }
 
-                            notify('ra.notification.updated', {
-                                type: 'info',
-                                messageArgs: { smart_count: 1 },
-                                undoable: mutationMode === 'undoable',
-                            });
-                            redirect(redirectTo, resource, data.id, data);
-                        },
-                        onError: onErrorFromSave
-                            ? onErrorFromSave
-                            : onError
-                            ? onError
-                            : (error: Error | string) => {
-                                  notify(
-                                      typeof error === 'string'
-                                          ? error
-                                          : error.message ||
-                                                'ra.notification.http_error',
-                                      {
-                                          type: 'error',
-                                          messageArgs: {
-                                              _:
-                                                  typeof error === 'string'
-                                                      ? error
-                                                      : error && error.message
-                                                      ? error.message
-                                                      : undefined,
-                                          },
-                                      }
-                                  );
-                              },
+                                if (onSuccess) {
+                                    return onSuccess(data, variables, context);
+                                }
+
+                                notify('ra.notification.updated', {
+                                    type: 'info',
+                                    messageArgs: { smart_count: 1 },
+                                    undoable: mutationMode === 'undoable',
+                                });
+                                redirect(redirectTo, resource, data.id, data);
+                            },
+                            onError: onErrorFromSave
+                                ? onErrorFromSave
+                                : onError
+                                ? onError
+                                : (error: Error | string) => {
+                                      notify(
+                                          typeof error === 'string'
+                                              ? error
+                                              : error.message ||
+                                                    'ra.notification.http_error',
+                                          {
+                                              type: 'error',
+                                              messageArgs: {
+                                                  _:
+                                                      typeof error === 'string'
+                                                          ? error
+                                                          : error &&
+                                                            error.message
+                                                          ? error.message
+                                                          : undefined,
+                                              },
+                                          }
+                                      );
+                                  },
+                        }
+                    );
+                } catch (error) {
+                    if ((error as HttpError).body.errors != null) {
+                        return (error as HttpError).body.errors;
                     }
-                );
+                }
             }),
         [
             id,